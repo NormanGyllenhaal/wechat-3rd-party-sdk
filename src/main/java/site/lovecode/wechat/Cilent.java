62904c6a78782e15b70595018e995fe3f53c60eb
NormanGyllenhaal <me@yangpeng.tech>
2016-07-17 17:49:41 +0800

    bean

--- src/main/java/site/lovecode/wechat/Cilent.java
+++
@@ -1,7 +0,0 @@
-package site.lovecode.wechat;
-
-/**
- * Created by yang on 2016/7/7.
- */
-public class Cilent {
-}
---
+++ src/main/java/site/lovecode/wechat/client/WechatFactory.java
@@ -0,0 +1,31 @@
+package site.lovecode.wechat.client;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * Created by Administrator on 2016/4/19.
+ */
+
+public class WechatFactory {
+
+
+	private final Logger logger = LoggerFactory.getLogger(WechatFactory.class);
+
+
+
+
+	/*public WechatClient getInstance( Long officialAccountId ) {
+		WechatConfig wechatConfig = JSON.parseObject(redisCache.getValue(officialAccountId), WechatConfig.class);
+		if ( wechatConfig.getAccountType().equals(OfficialAccountTypeEnum.AUTHORIZATION.key()) ) {
+			wechatAuthorizationClient.setWxMpConfigStorage(wechatConfig);
+			return wechatAuthorizationClient;
+		} else {
+			wechatBindClient.setWxMpConfigStorage(wechatConfig);
+			return wechatBindClient;
+		}
+	}
+*/
+}
